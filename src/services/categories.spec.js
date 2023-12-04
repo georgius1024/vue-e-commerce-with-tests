34f6fa7e3ec38f948522c73d48ecb6f7f9e92a9d
georgius1024 <georgius@ural.ru>
2023-12-04 09:10:37 +0500

    spy on

--- src/services/categories.spec.js
+++ src/services/categories.spec.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-
+import request from "@/services/request";
 import testSubject from '@/services/categories'
 
 vi.mock('@/services/request', () => ({
@@ -13,7 +13,9 @@ describe('categories service', () => {
     expect(testSubject).toBeTypeOf('function')
   })
   it('requests backend when called', async () => {
+    const quiery = vi.spyOn(request, 'get')
     const categories = await testSubject()
     expect(categories).toEqual(['category 1', 'category 2'])
+    expect(quiery).toBeCalled()
   })
 })
