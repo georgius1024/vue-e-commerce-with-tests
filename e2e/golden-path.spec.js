a1e32457f3438764b61fa1520d08104f739d1dca
Yurii Timofeev <georgius@ural.ru>
2024-06-06 19:39:51 +0500

    Fixed e2e tests

--- e2e/golden-path.spec.js
+++ e2e/golden-path.spec.js
@@ -5,10 +5,13 @@ test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/#/');
   await page.getByRole('link', { name: 'Beauty' }).click();
   await page.getByTestId('product-preview-1').getByRole('img', { name: 'preview' }).click();
+  await expect(page.getByTestId('checkout').first()).toHaveText('Your cart is empty');
   await page.getByRole('button', { name: 'Add to cart' }).click();
+  await expect(page.getByTestId('checkout').first()).toHaveText('1 product in your cart');
   await page.getByRole('button', { name: 'Remove from cart' }).click();
   await page.getByRole('button', { name: 'Add to cart' }).click();
-  await page.getByRole('link', { name: 'product in your cart:' }).click();
+  await expect(page.getByTestId('checkout').first()).toHaveText('1 product in your cart');
+  await page.getByRole('link', { name: 'product in your cart' }).click();
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,9 +3,9 @@
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand text-light">Home</router-link>
       <div class="flex-grow"></div>
-      <button v-if="!size" class="btn btn-secondary">Your cart is empty</button>
-      <RouterLink :to="{ name: 'checkout' }" v-else class="btn btn-secondary">
-        {{ size }} product in your cart:
+      <button v-if="!size" data-testid="checkout" class="btn btn-secondary">Your cart is empty</button>
+      <RouterLink :to="{ name: 'checkout' }" v-else data-testid="checkout" class="btn btn-secondary">
+        {{ size }} product in your cart
       </RouterLink>
     </div>
   </nav>
